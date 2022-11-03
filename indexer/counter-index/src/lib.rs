15d29653d7a515bb94229bfa492bbe18e1e2f552
atulpatare <atul.patare@launchventures.co>
2022-11-03 18:12:42 +0530

    added basic template for indexer

---
+++ indexer/counter-index/src/lib.rs
@@ -0,0 +1,32 @@
+
+#![allow(unused)]
+fn main() {
+extern crate alloc;
+use fuel_indexer_macros::indexer;
+
+#[indexer(manifest = "../../counter-index.manifest.yaml")]
+pub mod my_counter_index_module {
+
+    fn counter_module_handler_one(event: Count) {
+        let Count { count, id } = event;
+
+        let count_entity = match CountEntity::load(id) {
+            Some(o) => o,
+            None => CountEntity { id, count },
+        };
+
+        count_entity.save();
+
+        let CountEntity { id, count } = count_entity;
+
+        let adjusted_count_entity = AdjustedCountEntity{ 
+            id, 
+            count: count_entity.id, 
+            adjusted_count: count + 1
+        };
+
+        adjusted_count_entity.save();
+    }
+}
+
+}
