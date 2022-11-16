542bb45f67b69f5fe4944c02c99ca58925dde5db
atulpatare <atul.patare@launchventures.co>
2022-11-16 16:56:44 +0530

    updated file paths

--- counter-indexer/src/lib.rs
+++ counter-indexer/src/lib.rs
@@ -4,7 +4,24 @@ use fuel_indexer_macros::indexer;
 #[indexer(manifest = "./manifest.yaml")]
 pub mod my_counter_index_module {
 
-    fn counter_module_handler_one(_event: ValueUpdated) {
-        // todo: add entity manipulations
+    fn counter_module_handler_one(event: Count) {
+        let Count { count, id } = event;
+
+        let count_entity = match CountEntity::load(id) {
+            Some(o) => o,
+            None => CountEntity { id, count },
+        };
+
+        count_entity.save();
+
+        let CountEntity { id, count } = count_entity;
+
+        let adjusted_count_entity = AdjustedCountEntity { 
+            id, 
+            count: count_entity.id, 
+            adjusted_count: count + 1
+        };
+
+        adjusted_count_entity.save();
     }
 }
