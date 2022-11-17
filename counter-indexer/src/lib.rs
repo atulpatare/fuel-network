2623c06454bbd0575294082298d4f28d789543fa
atulpatare <atul.patare@launchventures.co>
2022-11-17 11:33:27 +0530

    updated indexer code

--- counter-indexer/src/lib.rs
+++ counter-indexer/src/lib.rs
@@ -5,24 +5,25 @@ use fuel_indexer_macros::indexer;
 pub mod my_counter_index_module {
 
     fn counter_module_handler_one(event: ValueUpdated) {
-        let ValueUpdated { counter } = event;
-        println!("running indexer for event with counter: {}", counter);
+        Logger::info("In the indexer function");
+        // let ValueUpdated { counter } = event;
+        println!("running indexer for event with counter");
 
-        let count_entity = match CountEntity::load(counter) {
-            Some(o) => o,
-            None => CountEntity { id: counter, count: counter },
-        };
+        // let count_entity = match CountEntity::load(counter) {
+        //     Some(o) => o,
+        //     None => CountEntity { id: counter, count: counter },
+        // };
 
-        count_entity.save();
+        // count_entity.save();
 
-        let CountEntity { id, count } = count_entity;
+        // let CountEntity { id, count } = count_entity;
 
-        let adjusted_count_entity = AdjustedCountEntity { 
-            id, 
-            count: count_entity.id, 
-            adjusted_count: count + 1
-        };
+        // let adjusted_count_entity = AdjustedCountEntity { 
+        //     id, 
+        //     count: count_entity.id, 
+        //     adjusted_count: count + 1
+        // };
 
-        adjusted_count_entity.save();
+        // adjusted_count_entity.save();
     }
 }
