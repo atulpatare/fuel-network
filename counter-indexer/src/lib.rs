3ad9e236648f5c9591221a51e9701910d1d83266
atulpatare <atul.patare@launchventures.co>
2022-12-02 19:33:39 +0530

    indexer clean up and code updates

--- counter-indexer/src/lib.rs
+++ counter-indexer/src/lib.rs
@@ -1,17 +1,51 @@
 extern crate alloc;
 use fuel_indexer_macros::indexer;
 
-#[indexer(manifest = "/home/atul/Projects/fuel-project/counter-indexer/manifest.yaml")]
+#[indexer(manifest = "manifest.yaml")]
 pub mod my_counter_index_module {
     fn increment_event_handler(event: IncrementEvent) {
         let IncrementEvent { id, count } = event;
-        let increment_entity = IncrementEntity { id, count: count.try_into().unwrap() };
+        let increment_entity = match Increment::load(id) {
+            Some(mut e) => {
+                e.count = count.try_into().unwrap();
+                e
+            },
+            None => Increment {
+                id,
+                count: count.try_into().unwrap()
+            }
+        };
         increment_entity.save();
+        save_count_entity(count);
     }
 
     fn decrement_event_handler(event: DecrementEvent) {
         let DecrementEvent { id, count } = event;
-        let decrement_entity = DecrementEntity { id, count: count.try_into().unwrap() };
+        let decrement_entity = match Decrement::load(id) {
+            Some(mut e) => {
+                e.count = count.try_into().unwrap();
+                e
+            },
+            None => Decrement {
+                id,
+                count: count.try_into().unwrap()
+            }
+        };
         decrement_entity.save();
+        save_count_entity(count);
+    }
+
+    fn save_count_entity(count: u64) {
+        let count_entity = match Count::load(1) {
+            Some(mut e) => {
+                e.count = count.try_into().unwrap();
+                e
+            }
+            None => Count {
+                id: 1,
+                count: count.try_into().unwrap()
+            }
+        };
+        count_entity.save();
     }
 }
