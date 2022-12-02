456e9f3d57e166034cbbe4857b6a4e649da6f313
atulpatare <atul.patare@launchventures.co>
2022-12-02 18:59:34 +0530

    refactored counter rust deps and code

--- counter-rust/src/main.rs
+++ counter-rust/src/main.rs
@@ -1,27 +1,11 @@
-#[macro_use]
-extern crate log;
+use std::str::FromStr;
 
-extern crate pretty_env_logger;
-
-use fuels_core::Identity;
-use fuels::signers::fuel_crypto::SecretKey;
 use fuels::prelude::{
     Bech32ContractId, Contract, Provider, TxParameters, WalletUnlocked,
 };
+use fuels::signers::fuel_crypto::SecretKey;
 use fuels_abigen_macro::abigen;
 use fuels_core::parameters::StorageConfiguration;
-use std::path::Path;
-use std::str::FromStr;
-extern crate rand; // 0.6.5
-
-use rand::Rng;
-
-pub fn tx_params() -> TxParameters {
-    let gas_price = 0;
-    let gas_limit = 1_000_000;
-    let byte_price = 0;
-    TxParameters::new(Some(gas_price), Some(gas_limit), Some(byte_price))
-}
 
 abigen!(
     Counter,
@@ -29,27 +13,21 @@ abigen!(
 );
 
 async fn get_contract_id(wallet: &WalletUnlocked) -> Bech32ContractId {
-    debug!("Creating new deployment for non-existent contract");
-
-    let _compiled =
-        Contract::load_contract("../counter/out/debug/counter.bin", &None).unwrap();
-
-    let bin_path = "../counter/out/debug/counter.bin".to_string();
     let contract_id = Contract::deploy(
-        &bin_path,
-        wallet,
-        tx_params(),
-        StorageConfiguration::default(),
+        "../counter/out/debug/counter.bin",
+        &wallet,
+        TxParameters::default(),
+        StorageConfiguration::with_storage_path(Some(
+            "../counter/out/debug/counter-storage_slots.json".to_string(),
+        )),
     )
-    .await
-    .unwrap();
+        .await
+        .unwrap();
 
     contract_id
 }
 
 async fn setup_provider_and_wallet(port: u16) -> (Provider, WalletUnlocked) {
-    let manifest_dir = env!("CARGO_MANIFEST_DIR");
-
     let address = format!("127.0.0.1:{}", port);
     let provider = Provider::connect(&address).await.unwrap();
 
@@ -64,16 +42,20 @@ async fn setup_provider_and_wallet(port: u16) -> (Provider, WalletUnlocked) {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    pretty_env_logger::init();
-
     let (_provider, wallet) = setup_provider_and_wallet(4000).await;
     let contract_id: Bech32ContractId = get_contract_id(&wallet).await;
-    info!("Using contract at {}", contract_id.to_string());
     let contract: Counter = Counter::new(contract_id, wallet);
 
-    // sending random no to increment with
-    let _ = contract.methods().increment(rand::thread_rng().gen_range(0..100)).call().await;
-    let _ = contract.methods().decrement(rand::thread_rng().gen_range(0..100)).call().await;
+    let count = contract.methods().count().call().await.unwrap().value;
+    let inc_res = contract.methods().increment(10).call().await;
+    assert_eq!(inc_res.is_err(), false);
+
+    let dec_res = contract.methods().decrement(5).call().await;
+    assert_eq!(dec_res.is_err(), false);
+    let updated = contract.methods().count().call().await.unwrap().value;
+
+    println!("Count initial :: {}", count);
+    println!("Count second :: {}", updated);
 
     Ok(())
 }
